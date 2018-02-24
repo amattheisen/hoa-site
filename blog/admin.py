2ae1a329bfe077662686050413463abf81635fd4
amattheisen <andrew.ctr.mattheisen@faa.gov>
2018-02-23 19:00:27 -0600

    more admin interface improvements

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
+from django import forms
 
 from .models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['category_title',
+                                         'category_description']}),
+    ]
+    inlines = [PostInline]
+    list_display = ('category_title', 'category_description')
+    search_fields = ['category_title']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('post_title', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['post_title', 'post_text']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django import forms
 
 
 class Category(models.Model):
@@ -13,7 +14,7 @@ class Category(models.Model):
 class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     post_title = models.CharField(max_length=100)
-    post_text = models.CharField(max_length=2500)
+    post_text = models.TextField(max_length=2500)
     pub_date = models.DateTimeField('Date published')
     likes = models.IntegerField(default=0)
 
@@ -23,3 +24,7 @@ class Post(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=31) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
