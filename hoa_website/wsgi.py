c2e8342b4bfde9e1b529e5d15ff78ef4f0f206eb
amattheisen <andrew.ctr.mattheisen@faa.gov>
2018-02-24 09:39:17 -0600

    rename project - part 1

--- hoa_website/wsgi.py
+++ hoa_website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hoa_website project.
+WSGI config for hoa-site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hoa_website.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hoa-site.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hoa_website.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hoa-site.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
